dc03e6527f538eb74660fc70dbe1fd9d66b401e7
Mike Warot <mike@warot.com>
2012-12-30 16:10:12 -0600

    comment was wrong... now working with 20 elements
    Signed-off-by: Mike Warot <mike@warot.com>

--- fountain_ideas.py
+++ fountain_ideas.py
@@ -1,6 +1,6 @@
 import random;
 
-def sampleset(x): # return 5 samples based on random seed x from 1000 elements
+def sampleset(x): # return 5 samples based on random seed x from 20 elements
 	random.seed(x)
 	return random.sample(xrange(20),5)
   
