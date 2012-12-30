6dd00c0b204b301ac781644df643df8314f56f9b
Mike Warot <mike@warot.com>
2012-12-30 17:18:31 -0600

    Expand packet lists into a binary array

--- fountain_ideas.py
+++ fountain_ideas.py
@@ -8,3 +8,13 @@ x = map(sampleset,range(20))
 y = map(sorted,x)
 z = sorted(y)
 
+def expand(a): # expand a sample list into a boolean array of 20 elements
+	def inside(q):
+		return (q in a)
+	
+	return map(inside,range(20))
+
+zz = map(expand,x)  # zz now has lists of true/false for each element\
+
+xor = lambda arg1, arg2 : arg1 ^ arg2
+
