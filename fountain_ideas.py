690108b7486a7dc98a318bba31f4a0e1ae550afc
Mike Warot <mike@warot.com>
2012-12-31 05:18:57 -0600

    working encode / decode algorithms, but decoding complexity is quite high

--- fountain_ideas.py
+++ fountain_ideas.py
@@ -1,12 +1,14 @@
 import random;
 
-K = 10
+K = 500    # how many blocks makes the whole set
+M = 7      # how many blocks do we Mix to get an output block? (should be ODD)
+Extras = 5 # how many extra samples?
 
-def sampleset(x): # return 5 samples based on random seed x from K elements
+def sampleset(x): # return M samples based on random seed x from K elements
 	random.seed(x)
-	return random.sample(xrange(K),5)
+	return random.sample(xrange(K),M)
   
-x = map(sampleset,range(K)) 
+x = map(sampleset,range(K+Extras)) 
 y = map(sorted,x)
 z = sorted(y)
 
@@ -25,7 +27,7 @@ listdiff = lambda arg1, arg2 : sorted(list(set(arg1)^set(arg2)))
 
 bracketn = lambda arg1 : [ arg1 ] # useful for generating lists of lists
 
-source = map(bracketn,xrange(K)) # [[0], [1], [2]...
+source = map(bracketn,xrange(K+Extras)) # [[0], [1], [2]...
 
 def swap2(a,b) : # used to swap rows in the Y table, to get a leading value
         source[a],source[b] = source[b],source[a]
@@ -35,5 +37,42 @@ def reduce2(a,b) : # used to factor out row b from row a
 	source[a] = listdiff(source[a],source[b])
 	y[a] = listdiff(y[a],y[b])
 
+def hasit(a) : # return a list of all entries in Y containing a
+	foundlist = list()
+	for i in range(len(y)):
+		if a in y[i] : foundlist.append(i)
+	return foundlist
+
+def findup(a) : # return a list of entries in y containing a, started at a
+	foundlist = list()
+	for i in range(a,len(y)):
+		if a in y[i] : foundlist.append(i)
+	return foundlist
+
+def findgreater_reduce(a) : # return a list of entries in y containing a, started after a AND reduce2 them
+	foundlist = list()
+	for i in range(a+1,len(y)):
+		if a in y[i] :
+			foundlist.append(i)
+			reduce2(i,a)
+	return foundlist
+
+def grindup(a) : # get entry a reduced from all greater entries
+	lst = findup(a)
+	if len(lst) >= 1 :
+		if lst[0] <> a :
+			swap2(a,lst[0])
+	return findgreater_reduce(a)
+
+def grinddown(a) : # reduce out all entries less than a
+	lst = hasit(a)
+	for i in lst:
+		if (i < a):
+			reduce2(i,a)
+
+
+
+map(grindup,range(K))            # forward elimination in the matrix
+map(grinddown,range(K-1,0,-1))   # factoring out single entries
 
 
